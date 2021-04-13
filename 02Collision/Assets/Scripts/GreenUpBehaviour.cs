03558c9b5f4a8022ef7810b5f806fdf3065be60f
Yusuf Pisan <pisan@uw.edu>
2021-04-12 17:32:29 -0700

    02Collision

--- 02Collision/Assets/Scripts/GreenUpBehaviour.cs
+++ 02Collision/Assets/Scripts/GreenUpBehaviour.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GreenUpBehaviour : MonoBehaviour
 {
+    public Text mEnemyCountText = null;
     public float speed = 10f;
-    public float mHeroRotateSpeed = 90f / 2f; // 90-degrees in 2 seconds
+    public float mHeroRotateSpeed = 100f / 2f; // 90-degrees in 2 seconds
     public bool mFollowMousePosition = true;
     // Start is called before the first frame update
+
+    private int mPlanesTouched = 0;
+
+    private GameController mGameGameController = null;
+
     void Start()
     {
-
+        mGameGameController = FindObjectOfType<GameController>();
     }
 
     // Update is called once per frame
@@ -26,7 +33,7 @@ public class GreenUpBehaviour : MonoBehaviour
         if (mFollowMousePosition)
         {
             pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("Position is " + pos);
+            // Debug.Log("Position is " + pos);
             pos.z = 0f;  // <-- this is VERY IMPORTANT!
             // Debug.Log("Screen Point:" + Input.mousePosition + "  World Point:" + p);
         }
@@ -61,5 +68,19 @@ public class GreenUpBehaviour : MonoBehaviour
             Debug.Log("Spawn Eggs:" + e.transform.localPosition);
         }
         transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Here x Plane: OnTriggerEnter2D");
+        mPlanesTouched = mPlanesTouched + 1;
+        mEnemyCountText.text = "Planes touched = " + mPlanesTouched;
+        Destroy(collision.gameObject);
+        mGameGameController.EnemyDestroyed();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) 
+    {
+        Debug.Log("Here x Plane: OnTriggerStay2D");
     }
 }
